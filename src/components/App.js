7b6de3f0c6314b55352e2aea1758bab1257365c7
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-09 13:53:49 -0500

    Create basic html based on mockup

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+const values = [
+  'Authenticity',
+  'Adventure',
+  'Achievement',
+  'Authority',
+]
+
 const App = () => (
-  <p>Hello Kai, you're an awesome guy.</p>
+  <section>
+    <header>
+      <h1>Discover your 10 core values</h1>
+      <p>It'll take less than 5 minutes</p>
+    </header>
+    <nav>
+      <ul>
+        <li>Sort Values</li>
+        <li>Your Top 10</li>
+      </ul>
+    </nav>
+    <section>
+      <ul>
+        { values.map(value => <li key={value}>{value}</li>) }
+      </ul>
+    </section>
+  </section>
 )
 
 export default App;
