c88814b7928df150425bdaf7c0bf2d2035b0fe07
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-10 18:42:50 -0500

    Tracking progress based on total items and items left

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import ProgressBar from './ProgressBar';
 
 const Footer = () => (
   <footer className="flex justify-between text-sm fixed left-0 bottom-0 bg-white w-full border-t-2 border-gray-400 px-6 py-4 md:hidden">
-    <ProgressBar />
+    <ProgressBar totalItems={50} itemsLeft={10} />
     <button className="bg-purple-600 disabled:opacity-50 disabled:cursor-not-allowed text-white font-medium p-2 rounded" disabled>Your Top 10</button>
   </footer>
 );
