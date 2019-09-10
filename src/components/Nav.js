30993521c234f4a61871f1bf9e50b5eb48a0c99c
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-10 17:21:58 -0500

    Remove some x padding on buttons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Nav = () => (
   <nav className="flex justify-around my-4">
-    <button className="w-1/2 bg-purple-200 font-medium py-1 px-8 rounded-full border-2 border-purple-600">
+    <button className="w-1/2 bg-purple-200 font-medium py-1 px-6 rounded-full border-2 border-purple-600">
       Sort Values
     </button>
-    <button className="w-1/2 font-medium py-1 px-8 rounded-full border-2 border-gray-300 hover:bg-purple-600 hover:border-purple-600 hover:text-white">
+    <button className="w-1/2 font-medium py-1 px-6 rounded-full border-2 border-gray-300">
       Your Top 10
     </button>
   </nav>
