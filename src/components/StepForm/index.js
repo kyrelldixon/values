d223c2f9993ca00bb584a15b2226e002860cc2d3
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-11 12:49:10 -0500

    Dynamically render step based on state

--- src/components/StepForm/index.js
+++ src/components/StepForm/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from '../Nav';
 import Container from '../Container';
 import Step1 from './Step1';
+import Step2 from './Step2';
 
-const StepForm = ({ values, toggleValue }) => (
-  <Container>
-    <Nav />
-    <form className="mx-auto max-w-md">
-      <Step1 values={values} toggleValue={toggleValue} />
-    </form>
-  </Container>
-);
+const StepForm = ({ values, toggleValue }) => {
+  const [step] = useState(1);
+  
+  const renderStep = () => {
+    return step === 1 ? 
+      <Step1 values={values} toggleValue={toggleValue} /> : 
+      <Step2 />
+  }
+
+  return (
+    <Container>
+      <Nav />
+      <form className="mx-auto max-w-md">
+        { renderStep() }
+      </form>
+    </Container>
+  );
+}
+
 
 export default StepForm;
