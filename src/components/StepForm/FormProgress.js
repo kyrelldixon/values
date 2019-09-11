42d8fe987a5500246c31efa0e86a76bf42204f72
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-11 12:52:54 -0500

    Refactor footer to form progress component

--- src/components/StepForm/FormProgress.js
+++ src/components/StepForm/FormProgress.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import ProgressBar from './ProgressBar';
+import ProgressBar from '../ProgressBar';
 
-const Footer = ({ values }) => {
+const FormProgress = ({ values }) => {
   const itemsLeft = values.filter(value => value.isCore === null);
   const coreValues = values.filter(value => value.isCore);
   const isDisabled = coreValues.length < 10;
   
   return (
-    <footer className="flex justify-between text-sm fixed left-0 bottom-0 bg-white w-full border-t-2 border-gray-400 px-6 py-4 md:hidden">
+    <div className="flex justify-between text-sm fixed left-0 bottom-0 bg-white w-full border-t-2 border-gray-400 px-6 py-4 md:hidden">
       <ProgressBar
         coreCount={coreValues.length}
         totalItems={values.length}
         itemsLeft={itemsLeft.length}
       />
       <button className="bg-purple-600 disabled:opacity-50 disabled:cursor-not-allowed text-white font-medium p-2 rounded" disabled={isDisabled}>Your Top 10</button>
-    </footer>
+    </div>
   );
 }
 
-export default Footer;
+export default FormProgress;
--- src/components/StepForm/index.js
+++ src/components/StepForm/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Nav from '../Nav';
 import Container from '../Container';
-import Footer from '../Footer';
+import FormProgress from './FormProgress';
 
 import Step1 from './Step1';
 import Step2 from './Step2';
@@ -21,7 +21,7 @@ const StepForm = ({ values, toggleValue }) => {
       <form className="mx-auto max-w-md">
         { renderStep() }
       </form>
-      <Footer values={values} />
+      <FormProgress values={values} />
     </Container>
   );
 }
