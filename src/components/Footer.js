7b298086875b5112b95e0612689e9f665e6bd1b2
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-11 10:29:55 -0500

    Button dynamic based on core value count

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
   const values = useContext(ValuesContext);
   const itemsLeft = values.filter(value => value.isCore === null);
   const coreValues = values.filter(value => value.isCore);
+  const isDisabled = coreValues.length < 10;
   
   return (
     <footer className="flex justify-between text-sm fixed left-0 bottom-0 bg-white w-full border-t-2 border-gray-400 px-6 py-4 md:hidden">
@@ -15,7 +16,7 @@ const Footer = () => {
         totalItems={values.length}
         itemsLeft={itemsLeft.length}
       />
-      <button className="bg-purple-600 disabled:opacity-50 disabled:cursor-not-allowed text-white font-medium p-2 rounded" disabled>Your Top 10</button>
+      <button className="bg-purple-600 disabled:opacity-50 disabled:cursor-not-allowed text-white font-medium p-2 rounded" disabled={isDisabled}>Your Top 10</button>
     </footer>
   );
 }
