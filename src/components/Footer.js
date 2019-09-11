ef032f587a541d0244843c29f2c0f7db1262eb9f
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-11 08:47:06 -0500

    Using Values context in footer for progress bar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ProgressBar from './ProgressBar';
 
-const Footer = () => (
-  <footer className="flex justify-between text-sm fixed left-0 bottom-0 bg-white w-full border-t-2 border-gray-400 px-6 py-4 md:hidden">
-    <ProgressBar totalItems={50} itemsLeft={10} />
-    <button className="bg-purple-600 disabled:opacity-50 disabled:cursor-not-allowed text-white font-medium p-2 rounded" disabled>Your Top 10</button>
-  </footer>
-);
+import ValuesContext from '../context/ValuesContext';
+
+const Footer = () => {
+  const values = useContext(ValuesContext);
+  
+  return (
+    <footer className="flex justify-between text-sm fixed left-0 bottom-0 bg-white w-full border-t-2 border-gray-400 px-6 py-4 md:hidden">
+      <ProgressBar totalItems={values.length} itemsLeft={values.length} />
+      <button className="bg-purple-600 disabled:opacity-50 disabled:cursor-not-allowed text-white font-medium p-2 rounded" disabled>Your Top 10</button>
+    </footer>
+  );
+}
 
 export default Footer;
