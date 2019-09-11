5604022248757267029acc3d6712fdd3f9800d46
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-11 08:48:29 -0500

    Add semi-colons

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,11 @@ import Container from './Container';
 import Footer from './Footer';
 
 import { ValuesProvider } from '../context/ValuesContext';
-import allValues from '../values.json'
+import allValues from '../values.json';
 
 const App = () => {
-  const [ values, ] = useState(allValues)
+  const [ values, ] = useState(allValues);
+  
   return (
     <Layout>
       <ValuesProvider value={values}>
