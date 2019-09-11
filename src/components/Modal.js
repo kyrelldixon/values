b1da66c690a905d661dd6e14fb42e996d662f2e9
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-11 16:24:15 -0500

    Made modal a section

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom';
 const Modal = ({ children, closeModal, open }) => (
   open
     ? ReactDOM.createPortal(
-      <div className="fixed bg-white p-6" style={{ left: '25%', top: '4rem' }}>
+      <section className="fixed bg-white p-6" style={{ left: '25%', top: '4rem' }}>
         <button onClick={closeModal}>&times;</button>
         {children}
-      </div>,
+      </section>,
       document.getElementById('root')
     )
     : null
