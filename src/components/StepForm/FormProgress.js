de27befd1f66d1c4e675da96181280a1dbc93eb7
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-12 18:57:18 -0500

    Update form progress to make visible on desktop

--- src/components/StepForm/FormProgress.js
+++ src/components/StepForm/FormProgress.js
@@ -8,19 +8,21 @@ const FormProgress = ({ values, goToStep2, openModal, currentStep }) => {
   const isDisabled = coreValues.length < 3;
   
   return (
-    <div className="flex justify-between text-sm fixed left-0 bottom-0 bg-white w-full border-t-2 border-gray-400 px-6 py-4 md:hidden">
-      <ProgressBar
-        coreCount={coreValues.length}
-        totalItems={values.length}
-        itemsLeft={itemsLeft.length}
-      />
-      <Button
-        primary
-        disabled={isDisabled}
-        onClick={ currentStep !== 2 ? goToStep2 : openModal }
-      >
-        { currentStep > 1 ? "Finish" : "Your Top 10" }
-      </Button>
+    <div className="text-sm fixed left-0 bottom-0 bg-white w-full border-t-2 border-gray-400 px-6 py-4">
+      <div className="flex justify-between max-w-2xl mx-auto">
+        <ProgressBar
+          coreCount={coreValues.length}
+          totalItems={values.length}
+          itemsLeft={itemsLeft.length}
+        />
+        <Button
+          primary
+          disabled={isDisabled}
+          onClick={ currentStep !== 2 ? goToStep2 : openModal }
+        >
+          { currentStep > 1 ? "Finish" : "Your Top 10" }
+        </Button>
+      </div>
     </div>
   );
 }
