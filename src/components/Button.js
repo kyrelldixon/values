c880e0921bed21e2527f3f6c52fd3e638e8bc142
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-12 18:18:47 -0500

    Refactor button css

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,8 @@
 import React from 'react';
 
-const Button = ({ disabled, onClick, primary, secondary, children, ...rest }) => {
-  if (primary && secondary) {
-    throw new Error("Button can only be primary or secondary");
-  }
-  else if (!primary && !secondary) {
-    primary = true;
-  }
-  
-  const primaryClassNames = primary ? "bg-purple-600 text-white" : "";
-  const secondaryClassName = secondary ? "border border-purple-600 text-purple-600" : "";
+const Button = ({ disabled, onClick, primary, secondary, children, ...rest }) => {  
+  const primaryClassNames = primary && "btn-primary";
+  const secondaryClassName = secondary && "btn-secondary";
   const className = `disabled:opacity-50 disabled:cursor-not-allowed font-medium p-2 rounded ${primaryClassNames} ${secondaryClassName}`
   return (
     <button
